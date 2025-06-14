0b410d3d65993cbdac030b8de3ac5265cbdd8106
Szeeto <patrasawali93@gmail.com>
2025-06-14 16:15:21 +0700

    commit

--- resources/views/components/textarea-trix.blade.php
+++ resources/views/components/textarea-trix.blade.php
@@ -1,4 +1,17 @@
 @props(['value','id','name'])
 <input id="{{ $id }}" type="hidden" name="{{ $name }}" value="{{ old($name, $value) }}">
 <trix-editor input="{{ $id }}"
-    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm min-h-80 text-white"></trix-editor>
+    class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm min-h-80 text-white"
+    ontrix-change="sanitizeTrixContent('{{ $id }}')"></trix-editor>
+
+@push('scripts')
+<script>
+function sanitizeTrixContent(id) {
+    let input = document.getElementById(id);
+    if (input) {
+        // Remove all <div> tags from the value
+        input.value = input.value.replace(/<\/?div[^>]*>/g, '');
+    }
+}
+</script>
+@endpush
